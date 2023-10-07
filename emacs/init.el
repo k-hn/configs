6444aed284c605420fb315bcd26cc9619cf50aac
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-07 00:48:19 +0000

    update emacs, add mpv

--- emacs/init.el
+++ emacs/init.el
@@ -18,12 +18,13 @@
   (load bootstrap-file nil 'nomessage))
 
 ;; Behaviour modifications
-(setq inhibit-startup-message t)
 (global-display-line-numbers-mode 1)
-(setq make-backup-files nil)
 (electric-pair-mode t)
 (global-hl-line-mode t)
 (load-theme 'modus-vivendi t)
+(setq inhibit-startup-message t)
+(setq make-backup-files nil)
+(setq x-select-enable-clipboard t)
 
 ;; Packages and package configurations
 (straight-use-package 'use-package)
@@ -34,37 +35,66 @@
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-l")
   (setq lsp-log-io nil)
-  (setq lsp-idle-delay 0.500)
+  (setq lsp-idle-delay 0.25)
   (setq lsp-use-plists t)
   (setq warning-suppress-types '((lsp-mode)))
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-doc-show-with-cursor t)
   ;; (setq lsp-keymap-prefix "s-l")
   :hook ((prog-mode . lsp-deferred)
-         ;; if you want which-key integration
+         ;; enable which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp lsp-deferred)
+
 (use-package lsp-ui
   :straight t
   :commands lsp-ui-mode)
-(use-package lsp-ivy
+
+(use-package helm-lsp
   :straight t
-  :commands lsp-ivy-workspace-symbol)
+  :commands helm-lsp-workspace-symbol)
+
 (use-package which-key
   :straight t
   :config
   (which-key-mode))
+
 (use-package company
-  :straight t)
+  :straight t
+  ;; :config
+  ;; (setq company-idle-delay 0.2)
+  )
+  ;; :hook (after-init . global-company-mode))
+
+(use-package company-box
+  :straight t
+  :hook (company-mode . company-box-mode))
+
 (use-package lsp-treemacs
-  :straight t)
+  :straight t
+  :bind (("M-o" . treemacs)))
+
 (use-package flycheck
   :straight t)
+
 (use-package dap-mode
   :straight t)
 
 (use-package geiser-guile
   :straight t)
+
 (use-package sml-mode
   :straight t)
+
+(use-package typescript-mode
+  :straight t
+  :config
+  (setq typescript-indent-level 2))
+
+(use-package js2-mode
+  :straight t
+  :config
+  (setq js2-basic-offset 2))
 
 (use-package pomm
   :straight t
@@ -86,3 +116,7 @@
   :straight t
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown"))
+
+(use-package doom-modeline
+  :straight t
+  :init (doom-modeline-mode 1))
