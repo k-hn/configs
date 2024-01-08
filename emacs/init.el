ac88b840cd64fd25a8fded3b2d07eea3baf48c0a
k-hn <125813796+k-hn@users.noreply.github.com>
2024-01-08 14:05:22 +0000

    update emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,14 @@
+;; optimising startup
+;;;;;;;;;;;;;;;;;;;;;
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
+;; straight package manager
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -55,6 +66,11 @@
 (use-package rainbow-delimiters
   :straight t
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Magit
+;;;;;;;;
+(use-package magit
+  :straight t)
 
 ;; Idris
 ;;;;;;;;
